e02727918e12f525df1375356e834315acd6dec9
Atabayev Ali <sptmbrw@gmail.com>
2025-04-01 18:41:01 +0500

    50%

---
+++ src/app-context-provider.js
@@ -0,0 +1,13 @@
+
+
+// export const AppContextProvider = ({ themeValue, userValue, appConfigValue }) => {
+//    return (
+//       <ThemeContext.Provider value={themeValue}>
+//          <UserDataContext.Provider value={userValue}>
+//             <AppConfigContext.Provider value={appConfigValue}>
+//                {сhildren}
+//             </AppConfigContext.Provider>
+//          </UserDataContext.Provider>
+//       </ThemeContext.Provider>
+//    )
+// }
--- src/app.js
+++ src/app.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import { Header, UserBlock } from './components';
+import { useEffect, useState } from 'react';
+import { AppContext } from './context';
+import styles from './app.module.css'
+
+
+const getUserFromServer = () => ({
+	id: 'a111',
+	name: 'Иван',
+	age: 23,
+	email: 'ivan@mail.ru',
+	phone: '+7-999-999-99-99',
+})
+
+const getAnotherUserFromServer = () => ({
+	id: 'a111',
+	name: 'Василий',
+	age: 23,
+	email: 'ivan@mail.ru',
+	phone: '+7-999-999-99-99',
+})
 
 export const App = () => {
+	const [userData, setUserData] = useState({});
+
+	const dispatch = (action) => {
+		const { type, payload } = action;
+
+		switch (type) {
+			case 'SET_USER_DATA': {
+				setUserData(payload);
+				break;
+			}
+			case 'SET_USER_AGE': {
+				setUserData({
+					...userData,
+					age: payload,
+				});
+				break;
+			}
+			default:
+		}
+	}
+
+	useEffect(() => {
+		const userDataFromServer = getUserFromServer();
+		setUserData(userDataFromServer);
+	}, []);
+
+	const onUserChange = () => {
+		const anotherUserDataFromServer = getAnotherUserFromServer();
+		setUserData(anotherUserDataFromServer);
+	}
+
+
 	return (
-		<div>
-			<div>
-				<h1>Заголовок приложения</h1>
-				<div>
-					{/* Добавьте ваш контент здесь */}
-				</div>
+		<AppContext.Provider value={{ userData, dispatch }}>
+			<div className={styles.app}>
+				<Header />
+				<hr />
+				<UserBlock />
+				<button onClick={onUserChange}>Сменить пользователя</button>
 			</div>
-		</div>
+		</AppContext.Provider>
 	);
 };
